69c7d4671b50ef0bfd1187dd0e13e2d772c32bfe
madpepper <hyper.pepper+github@gmail.com>
2018-08-09 11:32:37 +0900

    fix: macOS option is added.

--- dotfile_cp.sh
+++ dotfile_cp.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-cp .bash_profile ~/.
-cp .bashrc ~/.
-cp .vimrc ~/.
-cp .tmux.conf ~/.
-cp .tmux_weather.sh ~/.
-cp .gitconfig ~/.
+basePath=$(cd $(dirname $0); pwd)
+
+if [ "$1" = "OSX" ]; then
+    echo OSX
+    cp $basePath/.bashrc_osx ~/.bashrc
+else
+    echo LINUX
+    cp $basePath/.bashrc ~/.
+fi
+
+cp $basePath/.bash_profile ~/.
+cp $basePath/.vimrc ~/.
+cp $basePath/.tmux.conf ~/.
+cp $basePath/.tmux_weather.sh ~/.
+cp $basePath/.gitconfig ~/.
 
 echo "copied!"
 
